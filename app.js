08a91e718dcd029aa9b50f8dcf1780885253168c
marveeygoodlife <marveey8027@gmail.com>
2026-06-14 23:43:32 +0100

    Add section header and content upgraded nav style & paragraph size

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 "use strict";
+
 const toggleBtn = document.querySelector(".mobileMenuToggle");
 const ulLinks = document.querySelector(".navLinks");
+
+/* open nav menu */
 
 toggleBtn.addEventListener("click", () => {
     toggleBtn.classList.toggle("active");
     ulLinks.classList.toggle("active");
+    console.log("clicked!")
 })
